0d36e93a5aebc52a65c1ec65c8048878b1cf07b8
Anaghasan <anaghasanthosh80@gmail.com>
2023-09-24 03:26:14 +0530

    Initial commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,12 @@
+void main(){
+String name="Neha";
+int age    =20;
+int phnno  =9748632118;
+double cgpa =9.1;
+String email = "neha70@gmail.com";
+print('My name is $name');
+print('Iam $age yrs old');
+print('My Phone number is $phnno');
+print('$cgpa is my Cgpa in graduation');
+print('My Email id is $email');
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main() {
+  print('My Name is:Anagha'); // string
+  print('Age:20'); // string
+  print('My Phnno is:9778473218'); // string
+  print('CGPA:9.2');
+}
